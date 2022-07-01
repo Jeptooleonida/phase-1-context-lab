c6f4f304b8c48155f61fa195f423a9c263367bbd
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-01 06:40:23 +0300

    commit tree 80ae95ec5ea8f66e63d662c72db10da61070af5b

--- index.js
+++ index.js
@@ -1,23 +1,132 @@
 /* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecord(array){
+    const records ={
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return records
+}
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
+function createEmployeeRecords(array){
+    let newArr = []
+    array.forEach(arr=>{
+        newArr.push(createEmployeeRecord(arr))
+    })
+    return newArr
+}
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+  
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+  
+    return this
+  }
+  function createTimeOutEvent(Dataval)
+{
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(Dataval.slice(-4)),
+        date: Dataval.slice(0, 10)
+})
+return this
+}
+function hoursWorkedOnDate(Datainquiry)
+{
+    let InTime = this.timeInEvents.find(function(e){
+        return e.date === Datainquiry
     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    let OutTime = this.timeOutEvents.find(function(e){
+        return e.date === Datainquiry
+    })
+
+    return (OutTime.hour - InTime.hour) / 100
+}
+function wagesEarnedOnDate(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+  }
+  
+  
+  function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+  }
+  function wagesEarnedOnDate(Datainquiry)
+{
+    const Payunpaid = hoursWorkedOnDate(this, Datainquiry)* this.payPerHour
+    return parseFloat(Payunpaid.toString())
+}
+const allWagesFor = function () {
+    const verifiedDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+    const payable = verifiedDates.reduce(function (temp, d) {
+        return temp + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
 
     return payable
 }
+function calculatePayroll (arrayofObject)
+{
+    let sum=0;
+    for (let i=0;i<arrayofObject.length;i++) {
+        sum += allWagesFor.call(arrayofObject[i]);
+      }
+    return sum
+}
+function findEmployeeByFirstName(Array, firstName) {
+    return Array.find((Person) => Person.firstName === firstName )
+  }
+
+  function wagesEarnedOnDate(date = "all") {
+    return this.payPerHour * hoursWorkedOnDate.call(this, date);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*
+//  We're giving you this function. Take a look at it, you might see some usage
+//  that's new and different. That's because we're avoiding a well-known, but
+//  sneaky bug that we'll cover in the next few lessons!
+
+//  As a result, the lessons for this function will pass *and* it will be available
+//  for you to use if you need it!
+//  */
+
+// const allWagesFor = function () {
+//     const eligibleDates = this.timeInEvents.map(function (e) {
+//         return e.date
+//     })
+
+//     const payable = eligibleDates.reduce(function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+//     return payable
+// }
 
